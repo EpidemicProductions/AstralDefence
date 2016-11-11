3edef2fe1f2281db9df7d904f211ad6ee1d678aa
HomicidalNacho <tiger.collins@outlook.com>
2016-11-11 20:12:28 +1000

    Enemy Health and Item Drops
    IMPLEMENTED
    - Enemies health drops when hit by item with tag 'bullet'.
    - When health is 0, enemy destroys.
    - When enemy destroys pick up is dropped.
    - Pick up destroys after time.
    - Enemies destroy selves at the end of the pathway.

--- Assets/Scripts/Enemy/EnemyScript.cs
+++ Assets/Scripts/Enemy/EnemyScript.cs
@@ -13,6 +13,31 @@ public class EnemyScript : MonoBehaviour
     [SerializeField]
     private float health = 100f;
 
+    //Pickup
+    [SerializeField]
+    private GameObject coinPrefab;
+    [SerializeField]
+    private Transform spawnPoint;
+    [SerializeField]
+    private float destroyTime;
+
+
+    // When the enemy collides with another object
+    void OnCollisionEnter(Collision col)
+    {
+        //If the player hit an object with the bulltet tag
+        if (col.gameObject.tag == "Bullet")
+        {
+            health -= 10f;
+        }
+
+        // If the enemy makes it to the end (hits end wall), it will destroy
+        if (col.gameObject.name == "End of Pathway")
+        {
+            Destroy(gameObject);
+        }
+    }
+
     // Use this for initialization
     void Start()
     {
@@ -23,11 +48,30 @@ public class EnemyScript : MonoBehaviour
     void Update()
     {
         EnemyMove();
+        EnemyLife();
     }
 
-    //Moves the object to the object with the tag 'pathEnd'.
+    // Moves the object to the object that is set as the vairable of 'pathEnd'
     void EnemyMove()
     {
         agent.SetDestination(pathEnd.transform.position);
+    }
+
+    // Handles the enemies health and what happens upon hitting 0 health
+    void EnemyLife()
+    {
+        //If health is 0
+        if (health <= 0)
+        {
+            ItemDrop();
+            Destroy(gameObject);            
+        }
+    }
+
+    // Handles what item dropsand what it does for the player
+    void ItemDrop()
+    {
+        Rigidbody ElectricityDrop;
+        ElectricityDrop = Instantiate(coinPrefab, spawnPoint.position, spawnPoint.rotation) as Rigidbody;
     }
 }
