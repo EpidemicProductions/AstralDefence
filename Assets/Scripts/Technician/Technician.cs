e141a7348ae7de01645889ec13338ddf1edb1272
Nicholas Duxbury <110317@student.sae.edu.au>
2016-11-24 02:20:21 +1000

    basic connection scripted. fixes for bullets, tower placement

--- Assets/Scripts/Technician/Technician.cs
+++ Assets/Scripts/Technician/Technician.cs
@@ -5,17 +5,22 @@ public class Technician : MonoBehaviour
 {
     public GameObject technician;
     public ParticleSystem tap;
-    
 
+    public GameObject connectDisplay;
+
+    public GameObject turretIdentifier;
+    private Vector3 identifierHideLocation;
 
     Vector3 targetPosition;
 
     public float speed;
+    public RaycastHit storeHit;
 
     void Start()
     {
-
+        connectDisplay.SetActive(false);
         targetPosition = transform.position;
+        identifierHideLocation = new Vector3(0, 0, 0);
     }
     void Update()
     {
@@ -24,16 +29,21 @@ public class Technician : MonoBehaviour
         {
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             RaycastHit hit;
+            
 
             if (Physics.Raycast(ray, out hit))
             {
-                if (hit.collider.GetComponent<Turret>() ||
-                    hit.collider.GetComponent<Technician>() ||
+                storeHit = hit;
+                if (hit.collider.GetComponent<Technician>() ||
                     hit.collider.CompareTag("Prop"))
                 {
+                    if(connectDisplay.activeInHierarchy)
+                    {
+                        connectDisplay.SetActive(false);
+                    }
                     return;
                 }
-                else
+                else if(!hit.collider.GetComponent<Turret>())
                 {
 
                     targetPosition = hit.point + Vector3.up * 0.5f;
@@ -41,6 +51,20 @@ public class Technician : MonoBehaviour
                     Instantiate(tap, hit.point, Quaternion.Euler(90, 0, 0));
                     tap.Play();
                     Debug.Log(hit.collider.name);
+                    if (connectDisplay.activeInHierarchy)
+                    {
+                        connectDisplay.SetActive(false);
+                    }
+                }
+
+                if(hit.collider.GetComponent<Turret>())
+                {
+                    connectDisplay.SetActive(true);
+                    turretIdentifier.transform.position = hit.collider.transform.position;
+                }
+                else
+                {
+                    turretIdentifier.transform.position = identifierHideLocation;
                 }
                 
             }
@@ -57,4 +81,11 @@ public class Technician : MonoBehaviour
             other.GetComponent<Turret>().active = true;
         }
     }   
+
+    public void ConnectTower()
+    {
+        storeHit.collider.GetComponent<Turret>().active = true;
+        Debug.Log("im running");
+    }
+
 }
---
+++ Assets/Scripts/UI/Energy.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Energy : MonoBehaviour {
+    public Text resources;
+    public int energyVal;
+    public string energyDisp;
+    public ParticleSystem pickUp;
+
+    public GameObject stationPosition;
+    public GameObject towerPosition;
+    private float distance;
+
+    public Text connectCost;
+
+	// Use this for initialization
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        energyDisp = energyVal.ToString();
+        resources.text = energyDisp;
+        DetermineTurretCost();
+        Debug.Log(distance);
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "PickUp")
+        {
+            PickupEnergy();
+            Destroy(other.gameObject);
+            Instantiate(pickUp, transform.position, Quaternion.Euler(0, 0, 0));
+        }
+    }
+
+    void PickupEnergy()
+    {
+        energyVal += 10;
+    }
+
+    void DetermineTurretCost()
+    {
+        distance = Vector3.Distance(stationPosition.transform.position, towerPosition.transform.position);
+        float newDistance = distance * 10;
+        connectCost.text = newDistance.ToString("N0");
+    }
+}
