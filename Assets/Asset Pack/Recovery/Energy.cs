04f7f269ef6d22ea850a4a0b705585149a431bd2
Sholto Tiger Collins <tiger.collins@outlook.com>
2016-11-30 14:24:49 +1000

    Organised folders. Changed colouring of the connect button.

---
+++ Assets/Asset Pack/Recovery/Energy.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Energy : MonoBehaviour {
+    public int startingEnergy;
+    public Text resources;
+    public int energyVal;
+    public string energyDisp;
+    public ParticleSystem pickUp;
+
+    public GameObject stationPosition;
+    public GameObject towerPosition;
+    private float distance;
+
+    public int connectionCost;
+    public Text connectCost;
+
+	// Use this for initialization
+	void Start () {
+        energyVal = startingEnergy;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        energyDisp = energyVal.ToString();
+        resources.text = energyDisp;
+        DetermineTurretCost();
+        Debug.Log(distance);
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "PickUp")
+        {
+            PickupEnergy();
+            Destroy(other.gameObject);
+            Instantiate(pickUp, transform.position, Quaternion.Euler(0, 0, 0));
+        }
+    }
+
+    void PickupEnergy()
+    {
+        energyVal += 10;
+    }
+
+    void DetermineTurretCost()
+    {
+        distance = Vector3.Distance(stationPosition.transform.position, towerPosition.transform.position);
+        connectionCost = (int)distance  * 10;
+        connectCost.text = connectionCost.ToString("N0");
+    }
+}
